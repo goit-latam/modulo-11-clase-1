fd7a7f15a3c3eb8476c53e8b9fcb1ff968cb8863
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-06-13 16:35:04 +0000

    Deploying to gh-pages from @ goit-latam/modulo-11-clase-1@eb60c0199ffccb4910ab7ee95509dc0bff6c70d6 🚀

--- index.85a10e4d.js
+++
@@ -1,2 +0,0 @@
-fetch("".concat("http://localhost:3000","/users"),{method:"GET"}).then((function(o){if(!o.ok)throw new Error(o.status);return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log(o)}));
-//# sourceMappingURL=index.85a10e4d.js.map
--- index.da75c722.js
+++
@@ -1,2 +0,0 @@
-fetch("http://localhost:3000/users",{method:"GET"}).then((o=>{if(!o.ok)throw new Error(o.status);return o.json()})).then((o=>{console.log(o)})).catch((o=>{console.log(o)}));
-//# sourceMappingURL=index.da75c722.js.map
